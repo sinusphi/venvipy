24a226aeb86640b924742d97c90654f70aeed8ae
sinusphi <41879890+sinusphi@users.noreply.github.com>
2019-07-21 02:05:28 +0200

    add func get_venvs() and get_venvs_directory()

--- organize.py
+++ organize.py
@@ -3,6 +3,7 @@
 from subprocess import Popen, PIPE, CalledProcessError
 import os
 
+from dataclasses import dataclass
 
 
 
@@ -12,7 +13,7 @@ import os
 
 versFound, pathFound, notFound = [], [], []
 
-def getPyInstalls():
+def get_python_installs():
     """
     Get available Python 3 installations from common locations.
     """
@@ -48,6 +49,102 @@ def getPyInstalls():
 #] GET VENVS FROM DEFAULT DIRECTORY [#=======================================[#
 #]===========================================================================[#
 
+@dataclass
+class VenvInfo:
+    name: str
+    directory: str
+    version: str
+
+
+def get_venvs(path):
+    if not os.path.isdir(path):
+        return []
+
+    infos = []
+
+    for i, _dir in enumerate(os.listdir(path)):
+
+        bin_folder = os.path.join(path, _dir, "bin")
+        if not os.path.isdir(bin_folder):
+            continue
+
+        python_binary = os.path.join(bin_folder, "python")
+        if not os.path.isfile(python_binary):
+            continue
+
+        try:
+            res = Popen([python_binary, "-V"], stdout=PIPE, universal_newlines=True)
+            out, _ = res.communicate()
+            version = out.strip()
+            info = VenvInfo(_dir, path, version)
+            infos.append(info)
+
+        except Exception as err:
+            print(f"{err.args[1]} : [list index: {i} ] {python_binary}")
+
+    return infos
+
+
+def get_venvs_default():
+    """
+    Get the default venv directory.
+    """
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    default_file = os.path.join(current_dir, "def", "default")
+
+    if os.path.isfile(default_file):
+        with open(default_file, 'r') as f:
+            default_dir = f.read()
+            return get_venvs(default_dir)
+
+    return []
+
+
+get_python_installs()
+
+
+
+
+if __name__ == "__main__":
+
+    for venv in get_venvs_default():
+        print(venv.name, venv.version, venv.directory)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 venvDirs, venvVers, venvPath = [], [], []
 
 def getVenvs():
@@ -91,14 +188,6 @@ def getVenvs():
             venvPath.append(defDir)
 
 
-getPyInstalls()
+get_python_installs()
 getVenvs()
-
-
-
-if __name__ == "__main__":
-
-    for i in range(len(venvDirs)):
-        print(venvDirs[i])
-        print(venvVers[i])
-        print(venvPath[i])
+'''
