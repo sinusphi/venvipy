06aa03139c34e218dd4f0c0d116c01521a2e42bd
sinusphi <41879890+sinusphi@users.noreply.github.com>
2020-05-11 04:58:10 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     author="Youssef Serestou",
     author_email="youssef.serestou.83@gmail.com",
     url="https://github.com/sinusphi/venvipy",
-    download_url="https://github.com/sinusphi/venvipy/archive/v0.2.5.tar.gz",
+    download_url="https://github.com/sinusphi/venvipy/archive/v0.2.6.tar.gz",
     keywords=[
         "python",
         "python3",
