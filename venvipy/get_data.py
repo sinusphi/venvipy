ab371c360ef1fc66452d401367c5f39d07e3e2c8
sinusphi <41879890+sinusphi@users.noreply.github.com>
2020-09-03 21:41:29 +0200

    change version

--- venvipy/get_data.py
+++ venvipy/get_data.py
@@ -11,7 +11,7 @@ from subprocess import Popen, PIPE
 from dataclasses import dataclass
 
 
-__version__ = "1.0.1"
+__version__ = "0.3.2"
 
 CFG_DIR = os.path.expanduser("~/.venvipy")
 DB_FILE = os.path.expanduser("~/.venvipy/py-installs")
