6d0d9061a32554c7cf427b5bdf9b5297ad084a4a
sinusphi <sinusphi.sq@gmail.com>
2026-01-28 04:08:30 +0100

    increase __version__

--- venvipy/get_data.py
+++ venvipy/get_data.py
@@ -37,7 +37,7 @@ import requests
 
 from platforms import get_platform
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 CFG_DIR = Path.home() / ".venvipy"
 DB_FILE = Path.home() / ".venvipy" / "py-installs"
